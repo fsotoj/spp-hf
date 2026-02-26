1a3fd53670e953bacaf3d1e6e06820bf63d4454f
Felipe Soto <48763975+fsotoj@users.noreply.github.com>
2026-02-26 08:46:54 +0100

    bug fotos

--- global.R
+++ global.R
@@ -10,8 +10,8 @@ library(leaflet)
 library(classInt)
 library(shinyWidgets)
 library(shinyjs)
-#library(mapview)
-#library(webshot)
+# library(mapview)
+# library(webshot)
 library(jsonlite)
 library(tibble)
 library(purrr)
@@ -27,7 +27,7 @@ source("modules/map_module.R")
 source("modules/hc_line_module.R")
 source("fx/get_jstree_data.R")
 source("modules/camera_module_v2.R")
-#source("camera_module.R")
+# source("camera_module.R")
 source("modules/about_spp_module.R")
 source("modules/spp_list_module.R")
 
@@ -53,8 +53,8 @@ get_data <- function(file_name) {
 
 
 ui_assets <- c(
-  "agustina.webp", "francisco.webp", "guadalupe.webp", 
-  "felipe.webp", "sergio.webp", 
+  "agustina.webp", "francisco.webp", "guadalupe.webp",
+  "felipe.webp", "sergio.webp", "magdalena.webp",
   "EscuelaCienciasSocialesyGobierno_Horizontal_Blanco.webp",
   "EscuelaCienciasSocialesyGobierno_Horizontal_Negro.webp"
 )
@@ -69,46 +69,47 @@ for (asset in ui_assets) {
 }
 
 
-
-
 # get data ----------------------------------------------------------------
 
 
-data_info <- read.xlsx(get_data("dict_new.xlsx")) %>% 
+data_info <- read.xlsx(get_data("dict_new.xlsx")) %>%
   select(Category = category, Variable = variable, Description = description_for_ui)
 
-dict <- read.xlsx(get_data("dict_new.xlsx")) %>% 
+dict <- read.xlsx(get_data("dict_new.xlsx")) %>%
   filter(scope == "subnational")
 
-sled_names <- dict %>% 
-  filter(dataset == "Legislative Elections", viewable_map == 1) %>% pull(variable)
+sled_names <- dict %>%
+  filter(dataset == "Legislative Elections", viewable_map == 1) %>%
+  pull(variable)
 
 
 NED <- read.xlsx(get_data("NED (v.0.1).xlsx")) %>%
-  mutate(ideo_party_nat_exe = as.double(ideo_party_nat_exe),
-         start_date_head_nat_exe = as.Date(start_date_head_nat_exe - 2, origin = "1900-01-01"),
-         end_date_head_nat_exe   = as.Date(end_date_head_nat_exe - 2, origin = "1900-01-01"))
+  mutate(
+    ideo_party_nat_exe = as.double(ideo_party_nat_exe),
+    start_date_head_nat_exe = as.Date(start_date_head_nat_exe - 2, origin = "1900-01-01"),
+    end_date_head_nat_exe = as.Date(end_date_head_nat_exe - 2, origin = "1900-01-01")
+  )
 
 SEED <- read.xlsx(get_data("SEED SHINY (v.0.1).xlsx"))
-SED  <- read.xlsx(get_data("SED (v.0.1).xlsx"))
+SED <- read.xlsx(get_data("SED (v.0.1).xlsx"))
 SLED <- read.xlsx(get_data("SLED_plus_mex.xlsx"))
 SLED_ARG <- read.xlsx(get_data("SLED_ARG.xlsx"))
-SDI  <- read.xlsx(get_data("SDI (v.1).xlsx")) 
+SDI <- read.xlsx(get_data("SDI (v.1).xlsx"))
 
-cols_to_fill <- c("chamber_sub_leg",as.vector(outer(setdiff(sled_names,c("chamber_sub_leg","concurrent_election_with_nat_sub_leg")), c("_1","_2"), paste0)))
+cols_to_fill <- c("chamber_sub_leg", as.vector(outer(setdiff(sled_names, c("chamber_sub_leg", "concurrent_election_with_nat_sub_leg")), c("_1", "_2"), paste0)))
 
 SLED_wide <- SLED %>%
   select(country_state_code, year, chamber_election_sub_leg, all_of(sled_names)) %>%
   distinct() %>%
   pivot_wider(
-    id_cols     = c(country_state_code, year,chamber_sub_leg),
+    id_cols     = c(country_state_code, year, chamber_sub_leg),
     names_from  = chamber_election_sub_leg,
-    values_from = c(all_of(sled_names),-chamber_sub_leg),
+    values_from = c(all_of(sled_names), -chamber_sub_leg),
     names_glue  = "{.value}_{chamber_election_sub_leg}",
-    #values_fn   = ~ dplyr::last(., na_rm = TRUE),   # collapse duplicates
-    values_fill = NA                                 # fill absent cells with NA
+    # values_fn   = ~ dplyr::last(., na_rm = TRUE),   # collapse duplicates
+    values_fill = NA # fill absent cells with NA
   ) %>%
-  arrange(country_state_code, year)%>%
+  arrange(country_state_code, year) %>%
   # carry-forward within each chamber separately
   group_by(country_state_code) %>%
   complete(year = seq(min(year, na.rm = TRUE), max(year, na.rm = TRUE), 1)) %>%
@@ -121,22 +122,21 @@ SLED_wide <- SLED %>%
   ungroup()
 
 
-data <- left_join(NED,SED,c("country_name","country_code","year")) %>% 
-  left_join(.,SEED,c("country_state_code","year"))  %>%
+data <- left_join(NED, SED, c("country_name", "country_code", "year")) %>%
+  left_join(., SEED, c("country_state_code", "year")) %>%
   select(-matches("\\.y$")) %>%
-  rename_with(~ gsub("\\.x$", "", .x), ends_with(".x")) %>% 
-  left_join(.,SLED_wide,c("country_state_code","year"))  %>%
+  rename_with(~ gsub("\\.x$", "", .x), ends_with(".x")) %>%
+  left_join(., SLED_wide, c("country_state_code", "year")) %>%
   select(-matches("\\.y$")) %>%
-  rename_with(~ gsub("\\.x$", "", .x), ends_with(".x")) %>% 
-  left_join(.,SDI,c("country_state_code","year"))  %>%
+  rename_with(~ gsub("\\.x$", "", .x), ends_with(".x")) %>%
+  left_join(., SDI, c("country_state_code", "year")) %>%
   select(-matches("\\.y$")) %>%
   rename_with(~ gsub("\\.x$", "", .x), ends_with(".x"))
 
 
-
 geom <- st_read(get_data("geom_simple_maps.geojson"))
 
-party_colors     <- read.xlsx(get_data("party_colors.xlsx"))
+party_colors <- read.xlsx(get_data("party_colors.xlsx"))
 party_colors_leg <- read.xlsx(get_data("party_colors_leg_v3.xlsx"))
 
 
